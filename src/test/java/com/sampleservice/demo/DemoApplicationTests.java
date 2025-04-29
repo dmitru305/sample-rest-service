dadf87b14135158e25181298dda7a9fe53b62534
dmitru305 <dmitru305@users.noreply.github.com>
2025-04-29 19:54:10 +0000

    QA Assistance: JUnit tests updated

--- src/test/java/com/sampleservice/demo/DemoApplicationTests.java
+++ src/test/java/com/sampleservice/demo/DemoApplicationTests.java
@@ -9,8 +9,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        // Verify that the application context loads successfully
+    }
 
+    @Test
+    public void testMainMethod() {
+        // Test that the main method initializes without throwing an exception
+        boolean exceptionThrown = false;
+        try {
+            DemoApplication.main(new String[] {});
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        if (exceptionThrown) {
+            org.junit.Assert.fail("Exception was thrown during main method execution");
+        }
+    }
 }
